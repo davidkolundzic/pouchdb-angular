b94c23fc0832adc56ddc297ef72c5bc27cfdcfd6
David Kolundzic <david_k42@hotmail.com>
2020-03-10 21:22:27 +0100

    crud

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CRUDComponent } from './crud/crud.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CRUDComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/data.ts
@@ -0,0 +1,13 @@
+export const JOB = {
+    _id: 'job_001',
+    serviceDate: '2018-02-03',
+    customer: '834',
+    technician: {
+        EmployeId: '1023',
+        name: 'Sheriff, Paul'
+    },
+    workDone: [{
+        description: 'Driveway repair',
+        price: 225
+    }]
+};
